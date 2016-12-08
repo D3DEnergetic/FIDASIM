a1e972c857579894c809c2cc3fac3f1c8aaaf1bc
nbolte <nbolte@trialphaenergy.com>
2016-12-07 18:46:57 -0800

    stopped check_spec from print warning for every chord that misses grid. this fills terminal memory for cameras (many chords). now a short statement.

--- lib/python_prefida/check_spec.py
+++ lib/python_prefida/check_spec.py
@@ -12,30 +12,26 @@ import numpy as np
 
 
 def check_spec(inputs, chords):
-    """
-    #check_spec
-    Check if spectral geometry structure is valid
-    ***
-    ##Input Arguments
-         **inputs**: input structure
-
-         **chords**: spectral geometry structure
-
-    ##Example Usage
-    ```idl
-    IDL> check_spec, inputs, chords
-    ```
-    """
-    err_status = False
+    #+#check_spec
+    #+Check if spectral geometry structure is valid
+    #+***
+    #+##Input Arguments
+    #+     **inputs**: input structure
+    #+
+    #+     **chords**: spectral geometry structure
+    #+
+    #+##Example Usage
+    #+```idl
+    #+IDL> check_spec, inputs, chords
+    #+```
+    err = False
     info('Checking FIDA/BES inputs...')
 
     chords_keys = list(chords.keys())
 
-#    w = where("nchan" == strlowcase(TAG_NAMES(chords)),nw)
-#    if nw == 0:
     if 'nchan' not in chords_keys:
         error('"nchan" is missing from the FIDA/BES geometry')
-        err_status = True
+        err = True
         error('Invalid FIDA/BES geometry. Exiting...', halt=True)
 
     nchan = chords['nchan']
@@ -52,28 +48,28 @@ def check_spec(inputs, chords):
     nchan_string = {'dims': [nchan],
                     'type': [str, np.str_, np.bytes_]}
 
+    three_nchan_float = {'dims': [3, nchan],
+                         'type': [float, np.float64]}
+
     schema = {'data_source': zero_string,
               'nchan': zero_long,
               'system': zero_string,
               'id': nchan_string,
-              'lens': {'dims': [3, nchan],
-                       'type': [float, np.float64]},
-              'axis': {'dims': [3, nchan],
-                       'type': [float, np.float64]},
+              'lens': three_nchan_float,
+              'axis': three_nchan_float,
               'sigma_pi': nchan_double,
               'spot_size': nchan_double,
               'radius': nchan_double}
 
-    err_status = check_dict_schema(schema, chords, desc="FIDA/BES geometry")
-    if err_status:
+    err = check_dict_schema(schema, chords, desc="FIDA/BES geometry")
+    if err:
         error('Invalid FIDA/BES geometry. Exiting...', halt=True)
 
-#    err_arr = np.zeros(nchan, dtype=int)
     cross_arr = np.zeros(nchan, dtype=int)
     uvw_lens = chords['lens']
     uvw_axis = chords['axis']
 
-    #ROTATE CHORDS INTO BEAM GRID COORDINATES
+    # ROTATE CHORDS INTO BEAM GRID COORDINATES
     xyz_lens = uvw_to_xyz(inputs['alpha'], inputs['beta'], inputs['gamma'], uvw_lens, origin=inputs['origin'])
     xyz_axis = uvw_to_xyz(inputs['alpha'], inputs['beta'], inputs['gamma'], uvw_axis)
 
@@ -82,31 +78,29 @@ def check_spec(inputs, chords):
     rc = np.array([inputs['xmin'], inputs['ymin'], inputs['zmin']], dtype=float) + 0.5 * dr
 
     for i in range(nchan):
-#        chan_str = strcompress(string(i),/remove_all)
         if np.abs(np.sum(uvw_axis[:, i] ** 2.) - 1.) > 1e-5:
             error('Invalid optical axis for chord "' + chords['id'][i] + '". Expected norm(axis) == 1')
             print(np.sum(uvw_axis[:, i] ** 2.) - 1.)
-#            err_arr[i] = 1
 
         # Check if viewing chord intersects beam grid
         length, r_enter, r_exit = aabb_intersect(rc, dr, xyz_lens[:, i], xyz_axis[:, i])
         if length <= 0.0:
-            warn('Chord "{}" does not cross the beam grid'.format(chords['id'][i]))
-#            warn('Chord "' + chords['id'][i] + '" does not cross the beam grid')
             cross_arr[i] = 1
 
-#    w = where(cross_arr == 0.0, nw, complement=ww, ncomplement=nww)
-#    print f='(i3," out of ",i3," chords crossed the beam grid")',nw,nchan
-    w = (cross_arr == 0)
-    nw = cross_arr[w].size
-    print('{} out of {} chords crossed the beam grid'.format(nw, nchan))
-    if nw == 0:
-#    if 1 not in cross_arr:
-        error('No channels intersect the beam grid')
-        err_status = True
+    wbad = (cross_arr == 1)
+    nbad = cross_arr[wbad].size
+    if nbad > 0:
+        warn('The following {} chords do not cross the beam grid:'.format(nbad))
+        warn('Chord ID: {}'.format(chords['id'][wbad]))
 
-#    GET_OUT:
-    if err_status:
+    wgood = (cross_arr == 0)
+    ngood = cross_arr[wgood].size
+    print('{} out of {} chords crossed the beam grid'.format(ngood, nchan))
+    if ngood == 0:
+        error('No channels intersect the beam grid')
+        err = True
+
+    if err:
         error('Invalid FIDA/BES geometry. Exiting...', halt=True)
     else:
         success('FIDA/BES geometry is valid')

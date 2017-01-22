d39d5edf0ab491ce6c6eb6eca088a54fd7210bd7
Luke Stagner <lstagner@uci.edu>
2017-01-21 23:52:11 -0800

    make travis_key a hidden file  so no one messes with it

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,7 @@ SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 echo $REPO
 echo $SSH_REPO
 
-openssl aes-256-cbc -K $encrypted_60fa3eb19c83_key -iv $encrypted_60fa3eb19c83_iv -in travis_key.enc -out travis_key -d
+openssl aes-256-cbc -K $encrypted_60fa3eb19c83_key -iv $encrypted_60fa3eb19c83_iv -in .travis_key.enc -out travis_key -d
 
 if [ ! -f $TRAVIS_BUILD_DIR/travis_key ]; then
     echo "Missing Travis Deploy Key"

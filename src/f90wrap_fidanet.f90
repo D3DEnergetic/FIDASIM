c7533ecfaf8ae525823d394fa26fb8c854ecd5ba
kgage <gagek@uci.edu>
2017-07-26 14:23:52 -0700

    post build wrap

---
+++ src/f90wrap_fidanet.f90
@@ -0,0 +1,147 @@
+! Module fidanet defined in file fidanet.f90
+
+subroutine f90wrap_settables
+    use fidanet, only: settables
+    implicit none
+    
+    call settables()
+end subroutine f90wrap_settables
+
+subroutine f90wrap_setplasma(dene, denp, denimp, te, ti)
+    use fidanet, only: setplasma
+    implicit none
+    
+    real(8), intent(in) :: dene
+    real(8), intent(in) :: denp
+    real(8), intent(in) :: denimp
+    real(8), intent(in) :: te
+    real(8), intent(in) :: ti
+    call setplasma(dene=dene, denp=denp, denimp=denimp, te=te, ti=ti)
+end subroutine f90wrap_setplasma
+
+subroutine f90wrap_setinputs(ai, ab, impq, tables_file)
+    use fidanet, only: setinputs
+    implicit none
+    
+    real(8), intent(in) :: ai
+    real(8), intent(in) :: ab
+    integer, intent(in) :: impq
+    character(150), intent(in) :: tables_file
+    call setinputs(ai=ai, ab=ab, impq=impq, tables_file=tables_file)
+end subroutine f90wrap_setinputs
+
+subroutine f90wrap_calcvn(i_type, eb, vn)
+    use fidanet, only: calcvn
+    implicit none
+    
+    integer, intent(in) :: i_type
+    real(8), intent(in) :: eb
+    real(8), dimension(3), intent(inout) :: vn
+    call calcvn(i_type=i_type, eb=eb, vn=vn)
+end subroutine f90wrap_calcvn
+
+subroutine f90wrap_setstates(newstates, states)
+    use fidanet, only: setstates
+    implicit none
+    
+    real(8), dimension(6), intent(in) :: newstates
+    real(8), dimension(6), intent(inout) :: states
+    call setstates(newstates=newstates, states=states)
+end subroutine f90wrap_setstates
+
+subroutine f90wrap_testcol(i_type, eb, dt, states, dens, photons)
+    use fidanet, only: testcol
+    implicit none
+    
+    integer, intent(in) :: i_type
+    real(8), intent(in) :: eb
+    real(8), intent(in) :: dt
+    real(8), dimension(6), intent(inout) :: states
+    real(8), dimension(6), intent(inout) :: dens
+    real(8), intent(out) :: photons
+    call testcol(i_type=i_type, eb=eb, dt=dt, states=states, dens=dens, &
+        photons=photons)
+end subroutine f90wrap_testcol
+
+subroutine f90wrap_fidanet__get__ai(f90wrap_ai)
+    use fidanet, only: fidanet_ai => ai
+    implicit none
+    real(8), intent(out) :: f90wrap_ai
+    
+    f90wrap_ai = fidanet_ai
+end subroutine f90wrap_fidanet__get__ai
+
+subroutine f90wrap_fidanet__set__ai(f90wrap_ai)
+    use fidanet, only: fidanet_ai => ai
+    implicit none
+    real(8), intent(in) :: f90wrap_ai
+    
+    fidanet_ai = f90wrap_ai
+end subroutine f90wrap_fidanet__set__ai
+
+subroutine f90wrap_fidanet__get__ab(f90wrap_ab)
+    use fidanet, only: fidanet_ab => ab
+    implicit none
+    real(8), intent(out) :: f90wrap_ab
+    
+    f90wrap_ab = fidanet_ab
+end subroutine f90wrap_fidanet__get__ab
+
+subroutine f90wrap_fidanet__set__ab(f90wrap_ab)
+    use fidanet, only: fidanet_ab => ab
+    implicit none
+    real(8), intent(in) :: f90wrap_ab
+    
+    fidanet_ab = f90wrap_ab
+end subroutine f90wrap_fidanet__set__ab
+
+subroutine f90wrap_fidanet__array__states(dummy_this, nd, dtype, dshape, dloc)
+    use libfida
+    use fidanet, only: fidanet_states => states
+    implicit none
+    integer, intent(in) :: dummy_this(2)
+    integer, intent(out) :: nd
+    integer, intent(out) :: dtype
+    integer, dimension(10), intent(out) :: dshape
+    integer*8, intent(out) :: dloc
+    
+    nd = 1
+    dtype = 12
+    dshape(1:1) = shape(fidanet_states)
+    dloc = loc(fidanet_states)
+end subroutine f90wrap_fidanet__array__states
+
+subroutine f90wrap_fidanet__array__dens(dummy_this, nd, dtype, dshape, dloc)
+    use libfida
+    use fidanet, only: fidanet_dens => dens
+    implicit none
+    integer, intent(in) :: dummy_this(2)
+    integer, intent(out) :: nd
+    integer, intent(out) :: dtype
+    integer, dimension(10), intent(out) :: dshape
+    integer*8, intent(out) :: dloc
+    
+    nd = 1
+    dtype = 12
+    dshape(1:1) = shape(fidanet_dens)
+    dloc = loc(fidanet_dens)
+end subroutine f90wrap_fidanet__array__dens
+
+subroutine f90wrap_fidanet__get__photons(f90wrap_photons)
+    use fidanet, only: fidanet_photons => photons
+    implicit none
+    real(8), intent(out) :: f90wrap_photons
+    
+    f90wrap_photons = fidanet_photons
+end subroutine f90wrap_fidanet__get__photons
+
+subroutine f90wrap_fidanet__set__photons(f90wrap_photons)
+    use fidanet, only: fidanet_photons => photons
+    implicit none
+    real(8), intent(in) :: f90wrap_photons
+    
+    fidanet_photons = f90wrap_photons
+end subroutine f90wrap_fidanet__set__photons
+
+! End of module fidanet defined in file fidanet.f90
+
---
+++ src/fidanet.py
@@ -0,0 +1,233 @@
+from __future__ import print_function, absolute_import, division
+import _fidanet
+import f90wrap.runtime
+import logging
+
+class Fidanet(f90wrap.runtime.FortranModule):
+    """
+    Module fidanet
+    
+    
+    Defined at fidanet.f90 lines 2-108
+    
+    """
+    @staticmethod
+    def settables():
+        """
+        settables()
+        
+        
+        Defined at fidanet.f90 lines 19-23
+        
+        
+        """
+        _fidanet.f90wrap_settables()
+    
+    @staticmethod
+    def setplasma(dene, denp, denimp, te, ti):
+        """
+        setplasma(dene, denp, denimp, te, ti)
+        
+        
+        Defined at fidanet.f90 lines 25-38
+        
+        Parameters
+        ----------
+        dene : float
+        denp : float
+        denimp : float
+        te : float
+        ti : float
+        
+        """
+        _fidanet.f90wrap_setplasma(dene=dene, denp=denp, denimp=denimp, te=te, ti=ti)
+    
+    @staticmethod
+    def setinputs(ai, ab, impq, tables_file):
+        """
+        setinputs(ai, ab, impq, tables_file)
+        
+        
+        Defined at fidanet.f90 lines 40-52
+        
+        Parameters
+        ----------
+        ai : float
+        ab : float
+        impq : int
+        tables_file : str
+        
+        """
+        _fidanet.f90wrap_setinputs(ai=ai, ab=ab, impq=impq, tables_file=tables_file)
+    
+    @staticmethod
+    def calcvn(i_type, eb, vn):
+        """
+        calcvn(i_type, eb, vn)
+        
+        
+        Defined at fidanet.f90 lines 54-70
+        
+        Parameters
+        ----------
+        i_type : int
+        eb : float
+        vn : float array
+        
+        """
+        _fidanet.f90wrap_calcvn(i_type=i_type, eb=eb, vn=vn)
+    
+    @staticmethod
+    def setstates(newstates, states):
+        """
+        setstates(newstates, states)
+        
+        
+        Defined at fidanet.f90 lines 72-77
+        
+        Parameters
+        ----------
+        newstates : float array
+        states : float array
+        
+        """
+        _fidanet.f90wrap_setstates(newstates=newstates, states=states)
+    
+    @staticmethod
+    def testcol(i_type, eb, dt, states, dens):
+        """
+        photons = testcol(i_type, eb, dt, states, dens)
+        
+        
+        Defined at fidanet.f90 lines 79-103
+        
+        Parameters
+        ----------
+        i_type : int
+        eb : float
+        dt : float
+        states : float array
+        dens : float array
+        
+        Returns
+        -------
+        photons : float
+        
+        """
+        photons = _fidanet.f90wrap_testcol(i_type=i_type, eb=eb, dt=dt, states=states, \
+            dens=dens)
+        return photons
+    
+    @property
+    def ai(self):
+        """
+        Element ai ftype=real(8) pytype=float
+        
+        
+        Defined at fidanet.f90 line 10
+        
+        """
+        return _fidanet.f90wrap_fidanet__get__ai()
+    
+    @ai.setter
+    def ai(self, ai):
+        _fidanet.f90wrap_fidanet__set__ai(ai)
+    
+    @property
+    def ab(self):
+        """
+        Element ab ftype=real(8) pytype=float
+        
+        
+        Defined at fidanet.f90 line 11
+        
+        """
+        return _fidanet.f90wrap_fidanet__get__ab()
+    
+    @ab.setter
+    def ab(self, ab):
+        _fidanet.f90wrap_fidanet__set__ab(ab)
+    
+    @property
+    def states(self):
+        """
+        Element states ftype=real(8) pytype=float
+        
+        
+        Defined at fidanet.f90 line 13
+        
+        """
+        array_ndim, array_type, array_shape, array_handle = \
+            _fidanet.f90wrap_fidanet__array__states(f90wrap.runtime.empty_handle)
+        if array_handle in self._arrays:
+            states = self._arrays[array_handle]
+        else:
+            states = f90wrap.runtime.get_array(f90wrap.runtime.sizeof_fortran_t,
+                                    f90wrap.runtime.empty_handle,
+                                    _fidanet.f90wrap_fidanet__array__states)
+            self._arrays[array_handle] = states
+        return states
+    
+    @states.setter
+    def states(self, states):
+        self.states[...] = states
+    
+    @property
+    def dens(self):
+        """
+        Element dens ftype=real(8) pytype=float
+        
+        
+        Defined at fidanet.f90 line 14
+        
+        """
+        array_ndim, array_type, array_shape, array_handle = \
+            _fidanet.f90wrap_fidanet__array__dens(f90wrap.runtime.empty_handle)
+        if array_handle in self._arrays:
+            dens = self._arrays[array_handle]
+        else:
+            dens = f90wrap.runtime.get_array(f90wrap.runtime.sizeof_fortran_t,
+                                    f90wrap.runtime.empty_handle,
+                                    _fidanet.f90wrap_fidanet__array__dens)
+            self._arrays[array_handle] = dens
+        return dens
+    
+    @dens.setter
+    def dens(self, dens):
+        self.dens[...] = dens
+    
+    @property
+    def photons(self):
+        """
+        Element photons ftype=real(8) pytype=float
+        
+        
+        Defined at fidanet.f90 line 15
+        
+        """
+        return _fidanet.f90wrap_fidanet__get__photons()
+    
+    @photons.setter
+    def photons(self, photons):
+        _fidanet.f90wrap_fidanet__set__photons(photons)
+    
+    def __str__(self):
+        ret = ['<fidanet>{\n']
+        ret.append('    ai : ')
+        ret.append(repr(self.ai))
+        ret.append(',\n    ab : ')
+        ret.append(repr(self.ab))
+        ret.append(',\n    states : ')
+        ret.append(repr(self.states))
+        ret.append(',\n    dens : ')
+        ret.append(repr(self.dens))
+        ret.append(',\n    photons : ')
+        ret.append(repr(self.photons))
+        ret.append('}')
+        return ''.join(ret)
+    
+    _dt_array_initialisers = []
+    
+
+fidanet = Fidanet()
+

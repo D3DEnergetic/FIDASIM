3e954cff08972ff82f70ef912f8c2647f25dc909
TRAVIS-CI-for-Luke Stagner <lstagner@uci.edu>
2017-11-07 23:47:54 +0000

    master documentation updated by travis job 168.1 for commits 0ebd0e9e5438...9244761c4c19

--- master/src/fidasim.f90
+++ master/src/fidasim.f90
@@ -7811,7 +7811,7 @@ subroutine npa_mc
     type(FastIon) :: fast_ion
     real(Float64) :: phi,theta,dtheta
     real(Float64), dimension(3) :: ri, rf, rg, vi
-    integer :: det,j,ichan,ir,nrange
+    integer :: det,j,ichan,ir,nrange,it
     type(LocalEMFields) :: fields
     type(GyroSurface) :: gs
     real(Float64), dimension(nlevs) :: rates
@@ -7833,7 +7833,7 @@ subroutine npa_mc
     endif
 
     cnt=0.0
-    !$OMP PARALLEL DO schedule(guided) private(iion,iphi,ind,fast_ion,vi,ri,rf,phi,s,c,ir, &
+    !$OMP PARALLEL DO schedule(guided) private(iion,iphi,ind,fast_ion,vi,ri,rf,phi,s,c,ir,it, &
     !$OMP& randomu,rg,fields,uvw,uvw_vi,rates,states,flux,det,ichan,gs,nrange,gyrange,theta,dtheta)
     loop_over_fast_ions: do iion=1,particles%nparticle
         cnt=cnt+1
@@ -7894,7 +7894,18 @@ subroutine npa_mc
 
                         !! Store NPA Flux
                         flux = (dtheta/(2*pi))*sum(states)*beam_grid%dv
-                        call store_npa(det,ri,rf,vi,flux,fast_ion%class)
+                        spread_loop: do it=1,25
+                            theta = gyrange(1,ir) + (it-0.5)*dtheta/25
+                            call gyro_trajectory(gs, theta, ri, vi)
+                            call hit_npa_detector(ri, vi ,det, rf, det=ichan)
+                            if(det.ne.ichan) then
+                                if (inputs%verbose.ge.0)then
+                                    write(*,*) "NPA_MC: Missed Detector ",ichan
+                                endif
+                                cycle spread_loop
+                            endif
+                            call store_npa(det,ri,rf,vi,flux/25,fast_ion%class)
+                        enddo spread_loop
                     enddo gyro_range_loop
                 enddo detector_loop
             else !! Full Orbit

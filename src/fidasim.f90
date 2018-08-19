2e05ad5c8b3f8afeaa6211a219f170840b7fad5f
Alvin Garcia <alvin.garcia@uci.edu>
2018-08-19 16:25:19 -0400

    Reverted to original value

--- src/fidasim.f90
+++ src/fidasim.f90
@@ -797,7 +797,7 @@ type NPAResults
         !+ Number of particles that hit a detector
     integer(Int32) :: nmax = 1000000
         !+ Maximum allowed number of particles grows if necessary
-    integer(Int32) :: nenergy = 122
+    integer(Int32) :: nenergy = 100
         !+ Number of energy values
     type(NPAParticle), dimension(:), allocatable :: part
         !+ Array of NPA particles

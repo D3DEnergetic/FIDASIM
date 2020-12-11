d315e30ae653d6144e714122af76878f66de5a66
Shaun Haskey <shaskey@pppl.gov>
2020-12-10 19:15:57 -0500

    Make outputing the neutral reservoir optional to prevent very large output files using new input file switch output_reservoir

--- src/fidasim.f90
+++ src/fidasim.f90
@@ -997,6 +997,8 @@ type SimulationInputs
         !+ Total number of spectral switches on
     integer(Int32) :: load_neutrals
         !+ Load neutrals from file: 0 = off, 1=on
+    integer(Int32) :: output_reservoir
+        !+ Output neutral reservoir: 0 = off, 1=on
     integer(Int32) :: calc_npa
         !+ Calculate Active NPA: 0 = off, 1=on, 2=on++
     integer(Int32) :: calc_pnpa
@@ -1872,6 +1874,7 @@ subroutine read_inputs
     integer            :: calc_fida, calc_pfida, calc_npa, calc_pnpa
     integer            :: calc_birth,calc_fida_wght,calc_npa_wght
     integer            :: load_neutrals,verbose,flr,split,stark_components
+    integer            :: output_reservoir
     integer(Int64)     :: n_fida,n_pfida,n_npa,n_pnpa,n_nbi,n_halo,n_dcx,n_birth
     integer(Int32)     :: shot,nlambda,ne_wght,np_wght,nphi_wght,nlambda_wght
     real(Float64)      :: time,lambdamin,lambdamax,emax_wght
@@ -1888,7 +1891,7 @@ subroutine read_inputs
         calc_pfida, calc_npa, calc_pnpa,calc_birth, seed, flr, split, &
         calc_fida_wght, calc_npa_wght, load_neutrals, verbose, stark_components, &
         calc_neutron, n_fida, n_pfida, n_npa, n_pnpa, n_nbi, n_halo, n_dcx, n_birth, &
-        ab, pinj, einj, current_fractions, &
+        ab, pinj, einj, current_fractions, output_reservoir, &
         nx, ny, nz, xmin, xmax, ymin, ymax, zmin, zmax, &
         origin, alpha, beta, gamma, &
         ne_wght, np_wght, nphi_wght, &
@@ -1929,6 +1932,7 @@ subroutine read_inputs
     calc_fida_wght=0
     calc_npa_wght=0
     load_neutrals=0
+    output_reservoir=1
     verbose=0
     calc_neutron=0
     n_fida=0
@@ -2041,6 +2045,8 @@ subroutine read_inputs
 
     !! Misc. Settings
     inputs%load_neutrals=load_neutrals
+    inputs%output_reservoir=output_reservoir
+
     inputs%verbose=verbose
     inputs%flr = flr
     inputs%stark_components = stark_components
@@ -4481,27 +4487,28 @@ subroutine write_neutral_population(id, pop, error)
     enddo
 
     !Create reservoir group
-    call h5gcreate_f(id, "reservoir", gid, error)
-    call h5ltset_attribute_string_f(id,"reservoir","description", &
-         "Neutral Particle Reservoir",error)
+    if(inputs%output_reservoir.eq.1) then
+       call h5gcreate_f(id, "reservoir", gid, error)
+       call h5ltset_attribute_string_f(id,"reservoir","description", &
+            "Neutral Particle Reservoir",error)
 
-    call h5ltmake_dataset_int_f(gid,"k", 0, d, [maxval(pop%res%k)], error)
-    call h5ltset_attribute_string_f(gid,"k","description", &
-         "Reservoir Size", error)
+       call h5ltmake_dataset_int_f(gid,"k", 0, d, [maxval(pop%res%k)], error)
+       call h5ltset_attribute_string_f(gid,"k","description", &
+            "Reservoir Size", error)
 
-    call h5ltmake_compressed_dataset_int_f(gid, "n", 3, dims4(2:4), n, error)
-    call h5ltset_attribute_string_f(gid, "n", "description", &
-         "Number of particles in each reservoir", error)
+       call h5ltmake_compressed_dataset_int_f(gid, "n", 3, dims4(2:4), n, error)
+       call h5ltset_attribute_string_f(gid, "n", "description", &
+            "Number of particles in each reservoir", error)
 
-    call h5ltmake_compressed_dataset_double_f(gid, "w", 4, dims5(2:5), w, error, compress=.False.)
-    call h5ltset_attribute_string_f(gid, "w", "description", &
-         "Neutral Particle Weight", error)
+       call h5ltmake_compressed_dataset_double_f(gid, "w", 4, dims5(2:5), w, error, compress=.False.)
+       call h5ltset_attribute_string_f(gid, "w", "description", &
+            "Neutral Particle Weight", error)
 
-    call h5ltmake_compressed_dataset_double_f(gid, "v", 5, dims5, v, error, compress=.False.)
-    call h5ltset_attribute_string_f(gid,"v","units","cm/s",error)
-    call h5ltset_attribute_string_f(gid,"v","description", &
-         "Neutral Particle velocity in beam grid coordinates v(:,particle,i,j,k)", error)
-
+       call h5ltmake_compressed_dataset_double_f(gid, "v", 5, dims5, v, error, compress=.False.)
+       call h5ltset_attribute_string_f(gid,"v","units","cm/s",error)
+       call h5ltset_attribute_string_f(gid,"v","description", &
+            "Neutral Particle velocity in beam grid coordinates v(:,particle,i,j,k)", error)
+    endif
     !Close grid group
     call h5gclose_f(gid, error)
 

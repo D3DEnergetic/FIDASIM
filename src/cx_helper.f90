be84fbae53bb7847d74f8e9b0e8a2c1c48c8eca2
Lucas Morton <23484003+lamorton@users.noreply.github.com>
2021-01-16 09:05:49 -0800

    Switched to using C bindings to prevent name mangling and control variable types. Also fixing the array size from within Julia so that I can properly preallocate the array.

--- src/cx_helper.f90
+++ src/cx_helper.f90
@@ -1,24 +1,29 @@
 module cx_helper
     implicit none
+    private
+    public :: cx_matrix
     contains
-    subroutine cx_matrix(velocity,rates)
+    subroutine cx_matrix(velocity,nlevels,rates) bind(C,name="cx_matrix")!{{{
         !! Calculate the charge-exchange cross-section matrix for a given relative collision velocity in [cm/s].
         !! The matrix gives rates of the incoming (n) & outgoing (n') energy levels of the electron, up to n=6
         !! The rates are given in [cm^-3*s^-1]
         use libfida, only : SimulationInputs,bb_cx_rates,read_tables, nlevs,inputs,impurity_charge
+        use, intrinsic :: iso_c_binding, only: c_int, c_double
         implicit none
-        integer, parameter   :: Float64 = 8 !!Use 8-byte floats (ie, Float64)
-        real(Float64), intent(in) :: velocity
-        real(Float64), dimension(nlevs,nlevs), intent(out) :: rates !! catches the output from bb_cx_rates
+        real(c_double), intent(in) :: velocity
+        integer, intent(in) :: nlevels
+        real(c_double), dimension(nlevels,nlevels), intent(inout) :: rates !! catches the output from bb_cx_rates
         integer :: n_in !! incoming neutral energy level (principle quantum number)
-        real(Float64), dimension(nlevs) :: denn  !! density [cm^-3] of each energy state of neutrals
-        real(Float64), dimension(3) :: vn = [0.0,0.0,0.0] !! cm/s x,y,z components of velocity of neutral
-        real(Float64), parameter, dimension(3) :: vi = [0.0,0.0,0.0]!! velocity of ion
-       
+        real(c_double), dimension(nlevels) :: denn  !! density [cm^-3] of each energy state of neutrals
+        real(c_double), dimension(3) :: vn = [0.0,0.0,0.0] !! cm/s x,y,z components of velocity of neutral
+        real(c_double), parameter, dimension(3) :: vi = [0.0,0.0,0.0]!! velocity of ion
+        if (nlevels .gt. nlevs) then
+            print *,"nlevels must be less than ",nlevs
+            stop
+        endif 
         vn(1) = velocity
         impurity_charge = 6
-        
-        !!type(SimulationInputs) :: inputs !! needed to specify a few key things used in the rate calculator
+
         inputs%verbose = 1
         inputs%tables_file = '/Users/lmorton/Code/FIDASIM_lib/tables/atomic_tables.h5'
         inputs%calc_neutron = 0
@@ -27,9 +32,8 @@ module cx_helper
             denn = 0.0
             denn(n_in) = 1.0
             call bb_cx_rates(denn,vn,vi,rates(n_in,:))
-            print *,rates(n_in,:)
         enddo
         !! CX reaction rate in units [1/s] (ie, reactions per incident fast ion per second, use the fast ion velocity to get a decay length, or the fast ion density to get a volumetric rate)
         !! The rate has 'nlev' values, for each of the energy levels that the post-CX fast neutral could have
-    end subroutine cx_matrix
+    end subroutine cx_matrix!}}}
 end module cx_helper
--- src/cx_helper.jl
+++ src/cx_helper.jl
@@ -1,7 +1,6 @@
-
 function cx_matrix(velocity::Float64)
-    #convert velocity 
-    σ = Array{Float64,2}(undef, 6,6)
-    ccall((:__cx_helper_MOD_cx_matrix, "./cx_helper.so"), Cvoid, (Ptr{Float64},Ptr{Float64}), Ref(velocity),σ)
+    nlevs = Int32(6) #Maximum is 6, but can be lower
+    σ = Array{Float64,2}(undef,nlevs,nlevs)
+    ccall((:cx_matrix, "./cx_helper.so"), Cvoid, (Ptr{Float64},Ref{Int32},Ptr{Float64}), Ref(velocity),Ref(nlevs),σ)
     return σ
 end
